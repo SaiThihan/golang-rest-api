d8aa055e506b9ff5322d0da77f4d40bd2a96d369
Sai Thi Han <saithihan668@gmail.com>
2026-07-03 15:33:31 +0630

    fix: modify get all posts routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -12,12 +12,12 @@ func SetupRoutes(app *app.Application) *chi.Mux {
 		r.Use(app.Middleware.Authenticate)
 
 		r.With(app.Middleware.RequireAuthenticatedUser).Get("/posts/{id}", app.PostHandler.HandleGetPostById)
-		r.With(app.Middleware.RequireAuthenticatedUser).Get("/posts", app.PostHandler.HandleGetPosts)
+
 		r.With(app.Middleware.RequireAuthenticatedUser).Post("/posts", app.PostHandler.HandleCreatePost)
 		r.With(app.Middleware.RequireAuthenticatedUser).Put("/posts/{id}", app.PostHandler.HandleUpdatePost)
 		r.With(app.Middleware.RequireAuthenticatedUser).Delete("/posts/{id}", app.PostHandler.HandleDeletePost)
 	})
-
+	r.Get("/posts", app.PostHandler.HandleGetPosts)
 	r.Get("/health", app.HealthCheck)
 
 	r.Post("/register", app.UserHandler.HandleRegister)
